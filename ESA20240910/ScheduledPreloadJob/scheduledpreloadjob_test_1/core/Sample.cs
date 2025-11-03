842ac45bb4afb151c824e0b5b84c93a767e3b3b0
Alibaba Cloud SDK <sdk-team@alibabacloud.com>
2025-11-03 08:44:51 +0000

    Batch upload ScheduledPreloadJob SDK code samples, jobId: 599a8e79-67ad-48c3-bc3f-4dd9fea19a20.

---
+++ ESA20240910/ScheduledPreloadJob/scheduledpreloadjob_test_1/core/Sample.cs
@@ -0,0 +1,216 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Darabonba.Utils;
+using ESA20240910Client = AlibabaCloud.SDK.ESA20240910.Client;
+using AlibabaCloud.OpenApiClient.Models;
+using Aliyun.Credentials;
+using AlibabaCloud.SDK.ESA20240910.Models;
+using AlibabaCloud.TeaUtil;
+
+namespace AlibabaCloud.CodeSample
+{
+    public class Sample 
+    {
+
+        public Sample()
+        {
+        }
+
+
+        /// <term><b>Description:</b></term>
+        /// <description>
+        /// <para>Init Client</para>
+        /// </description>
+        public static ESA20240910Client CreateESA20240910Client()
+        {
+            Config config = new Config();
+            config.Credential = new Client(null);
+            // Endpoint please refer to https://api.aliyun.com/product/ESA
+            config.Endpoint = "esa.cn-hangzhou.aliyuncs.com";
+            return new ESA20240910Client(config);
+        }
+
+
+        public static async Task<PurchaseRatePlanResponseBody> RatePlanInstAsync(ESA20240910Client client)
+        {
+            Console.WriteLine("Begin Call PurchaseRatePlan to create resource");
+            PurchaseRatePlanRequest purchaseRatePlanRequest = new PurchaseRatePlanRequest
+            {
+                Type = "NS",
+                ChargeType = "PREPAY",
+                AutoRenew = false,
+                Period = 1,
+                Coverage = "overseas",
+                AutoPay = true,
+                PlanName = "high",
+            };
+            PurchaseRatePlanResponse purchaseRatePlanResponse = await client.PurchaseRatePlanAsync(purchaseRatePlanRequest);
+            DescribeRatePlanInstanceStatusRequest describeRatePlanInstanceStatusRequest = new DescribeRatePlanInstanceStatusRequest
+            {
+                InstanceId = purchaseRatePlanResponse.Body.InstanceId,
+            };
+            int? currentRetry = 0;
+            int? delayedTime = 10000;
+            int? interval = 10000;
+
+            while (currentRetry < 10) {
+                try
+                {
+                    int? sleepTime = 0;
+                    if (currentRetry == 0)
+                    {
+                        sleepTime = delayedTime;
+                    }
+                    else
+                    {
+                        sleepTime = interval;
+                    }
+                    Console.WriteLine("Polling for asynchronous results...");
+                    await Task.Delay(sleepTime.Value);
+                }
+                catch (Darabonba.Exceptions.DaraException error)
+                {
+                    throw new Darabonba.Exceptions.DaraException(new Dictionary<string, string>
+                    {
+                        {"message", error.Message},
+                    });
+                }
+                DescribeRatePlanInstanceStatusResponse describeRatePlanInstanceStatusResponse = await client.DescribeRatePlanInstanceStatusAsync(describeRatePlanInstanceStatusRequest);
+                string instanceStatus = describeRatePlanInstanceStatusResponse.Body.InstanceStatus;
+                if (instanceStatus == "running")
+                {
+                    Console.WriteLine("Call PurchaseRatePlan success, response: ");
+                    Console.WriteLine(Common.ToJSONString(purchaseRatePlanResponse));
+                    return purchaseRatePlanResponse.Body;
+                }
+                currentRetry++;
+            }
+            throw new Darabonba.Exceptions.DaraException(new Dictionary<string, string>
+            {
+                {"message", "Asynchronous check failed"},
+            });
+        }
+
+
+        public static async Task<CreateSiteResponseBody> SiteAsync(PurchaseRatePlanResponseBody ratePlanInstResponseBody, ESA20240910Client client)
+        {
+            Console.WriteLine("Begin Call CreateSite to create resource");
+            CreateSiteRequest createSiteRequest = new CreateSiteRequest
+            {
+                SiteName = "gositecdn.cn",
+                InstanceId = ratePlanInstResponseBody.InstanceId,
+                Coverage = "overseas",
+                AccessType = "NS",
+            };
+            CreateSiteResponse createSiteResponse = await client.CreateSiteAsync(createSiteRequest);
+            GetSiteRequest getSiteRequest = new GetSiteRequest
+            {
+                SiteId = createSiteResponse.Body.SiteId,
+            };
+            int? currentRetry = 0;
+            int? delayedTime = 60000;
+            int? interval = 10000;
+
+            while (currentRetry < 5) {
+                try
+                {
+                    int? sleepTime = 0;
+                    if (currentRetry == 0)
+                    {
+                        sleepTime = delayedTime;
+                    }
+                    else
+                    {
+                        sleepTime = interval;
+                    }
+                    Console.WriteLine("Polling for asynchronous results...");
+                    await Task.Delay(sleepTime.Value);
+                }
+                catch (Darabonba.Exceptions.DaraException error)
+                {
+                    throw new Darabonba.Exceptions.DaraException(new Dictionary<string, string>
+                    {
+                        {"message", error.Message},
+                    });
+                }
+                GetSiteResponse getSiteResponse = await client.GetSiteAsync(getSiteRequest);
+                string status = getSiteResponse.Body.SiteModel.Status;
+                if (status == "pending")
+                {
+                    Console.WriteLine("Call CreateSite success, response: ");
+                    Console.WriteLine(Common.ToJSONString(createSiteResponse));
+                    return createSiteResponse.Body;
+                }
+                currentRetry++;
+            }
+            throw new Darabonba.Exceptions.DaraException(new Dictionary<string, string>
+            {
+                {"message", "Asynchronous check failed"},
+            });
+        }
+
+
+        public static async Task<CreateScheduledPreloadJobResponseBody> SchedPreloadJobAsync(CreateSiteResponseBody siteResponseBody, ESA20240910Client client)
+        {
+            Console.WriteLine("Begin Call CreateScheduledPreloadJob to create resource");
+            CreateScheduledPreloadJobRequest createScheduledPreloadJobRequest = new CreateScheduledPreloadJobRequest
+            {
+                InsertWay = "oss",
+                SiteId = siteResponseBody.SiteId,
+                OssUrl = "https://yandanpub.oss-cn-hangzhou.aliyuncs.com/1.txt",
+                Name = "testscheduledpreloadjob",
+            };
+            CreateScheduledPreloadJobResponse createScheduledPreloadJobResponse = await client.CreateScheduledPreloadJobAsync(createScheduledPreloadJobRequest);
+            Console.WriteLine("Call CreateScheduledPreloadJob success, response: ");
+            Console.WriteLine(Common.ToJSONString(createScheduledPreloadJobResponse));
+            return createScheduledPreloadJobResponse.Body;
+        }
+
+
+        public static async Task DestroySchedPreloadJobAsync(CreateScheduledPreloadJobResponseBody createScheduledPreloadJobResponseBody, ESA20240910Client client)
+        {
+            Console.WriteLine("Begin Call DeleteScheduledPreloadJob to destroy resource");
+            DeleteScheduledPreloadJobRequest deleteScheduledPreloadJobRequest = new DeleteScheduledPreloadJobRequest
+            {
+                Id = createScheduledPreloadJobResponseBody.Id,
+            };
+            DeleteScheduledPreloadJobResponse deleteScheduledPreloadJobResponse = await client.DeleteScheduledPreloadJobAsync(deleteScheduledPreloadJobRequest);
+            Console.WriteLine("Call DeleteScheduledPreloadJob success, response: ");
+            Console.WriteLine(Common.ToJSONString(deleteScheduledPreloadJobResponse));
+        }
+
+
+        /// <term><b>Description:</b></term>
+        /// <description>
+        /// <para>Running code may affect the online resources of the current account, please proceed with caution!</para>
+        /// </description>
+        public static async Task Main(string[] args)
+        {
+            // The code may contain api calls involving fees. Please ensure that you fully understand the charging methods and prices before running.
+            // Set the environment variable COST_ACK to true or delete the following judgment to run the sample code.
+            string costAcknowledged = Environment.GetEnvironmentVariable("COST_ACK");
+            if (costAcknowledged.IsNull() || !(costAcknowledged == "true"))
+            {
+                Console.WriteLine("Running code may affect the online resources of the current account, please proceed with caution!");
+                return ;
+            }
+            // Init client
+            ESA20240910Client esa20240910Client = CreateESA20240910Client();
+            // Init resource
+            //resource_RatePlanInstance_ScheduledPreloadJob_test_1
+            PurchaseRatePlanResponseBody ratePlanInstRespBody = await RatePlanInstAsync(esa20240910Client);
+            //resource_Site_ScheduledPreloadJob_test_1
+            CreateSiteResponseBody siteRespBody = await SiteAsync(ratePlanInstRespBody, esa20240910Client);
+            CreateScheduledPreloadJobResponseBody schedPreloadJobRespBody = await SchedPreloadJobAsync(siteRespBody, esa20240910Client);
+            // destroy resource
+            await DestroySchedPreloadJobAsync(schedPreloadJobRespBody, esa20240910Client);
+        }
+
+    }
+}
+
